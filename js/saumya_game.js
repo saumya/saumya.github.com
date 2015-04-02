f05571482b490558c0c97b1bd0169b248cf60b90
saumya <2saumyaray@gmail.com>
2015-04-03 03:09:09 +0530

    WIP, setting the game

--- js/saumya_game.js
+++ js/saumya_game.js
@@ -23,6 +23,8 @@
 // http://opengameart.org/content/glitch-sprite-assets-huge-collection
 	var gameEngine = {
 		game : null,
+		frameCounter: 2,
+		btnHome : null,
 		start : function(){
 			console.log('Game Engine : Start');
 			var a = $("#gameX");
@@ -32,17 +34,27 @@
 		},
 		preload : function(){
 			console.log('Phaser : preload');
-			this.game.load.image('buttons', 'img/game_ui_buttons.png');
+			//this.game.load.image('buttons', 'img/game_ui_buttons.png');
+			//this.game.load.spritesheet('buttons', 'img/game_ui_buttons.png',100,100);
+			this.game.load.spritesheet('buttons', 'img/game_ui_buttons.png',34.5,34.5);
 		},
 		create : function(){
 			console.log('Phaser : create');
-			this.game.add.sprite(100, 100, 'buttons');
+			var _this = this;
+			//this.btnHome = this.game.add.sprite(10,10,'buttons',12);
+			//this.btnHome.animations.add('btnHomeAnim',[12,13,14,15],1,true,true);
+			//
+			//var button = this.game.add.button(this.game.world.centerX - 95, 400, 'buttons', null, this, over, out, down,up);
+			this.btnHome = this.game.add.button(10, 10, 'buttons',this.onHomeClick,this,1,3,2,0);
 		},
 		update : function(){
-			console.log('Phaser : update');
+			//this.btnHome.animations.play('btnHomeAnim');
 		},
 		end : function(){
 			console.log('Game Engine : End');
+		},
+		onHomeClick: function(){
+			console.log('home click');
 		}
 	};
 })();
