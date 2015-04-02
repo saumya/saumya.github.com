613ebf130c02120b497b887df48ef8f514a576df
saumya <2saumyaray@gmail.com>
2015-04-02 18:36:15 +0530

    ADDed : game buttons

--- js/saumya_game.js
+++ js/saumya_game.js
@@ -2,7 +2,8 @@
 (function(){
 	console.log('Game On !');
 	//var aURL = "https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js";
-	var pahserURL = "https://cdnjs.cloudflare.com/ajax/libs/phaser/2.3.0/phaser.min.js";
+	var pahserURL = "js/vendor/phaser.js";
+	//var pahserURL = "https://cdnjs.cloudflare.com/ajax/libs/phaser/2.3.0/phaser.min.js";
 	$.getScript( pahserURL )
 	  .done(function( script, textStatus ) {
 	    console.log( 'SUCCESS : Phaser Load',textStatus );
@@ -16,19 +17,26 @@
         console.log('exception',exception);
         $('#idGameOn').text('ERROR : Loading Enigne. Please, Try Again.');
 	});
+// Ref :
+// SpriteSheet : 
+// http://www.gameart2d.com/freebies.html
+// http://opengameart.org/content/glitch-sprite-assets-huge-collection
 	var gameEngine = {
+		game : null,
 		start : function(){
 			console.log('Game Engine : Start');
 			var a = $("#gameX");
 			var wX = a.width();
 			var hX = 400;
-			var game = new Phaser.Game(wX, hX, Phaser.AUTO, 'gameX', { preload: this.preload, create: this.create, update: this.update });
+			this.game = new Phaser.Game(wX, hX, Phaser.AUTO, 'gameX', { preload: this.preload, create: this.create, update: this.update });
 		},
 		preload : function(){
 			console.log('Phaser : preload');
+			this.game.load.image('buttons', 'img/game_ui_buttons.png');
 		},
 		create : function(){
 			console.log('Phaser : create');
+			this.game.add.sprite(100, 100, 'buttons');
 		},
 		update : function(){
 			console.log('Phaser : update');
